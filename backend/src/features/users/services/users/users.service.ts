ec4c9c28d6d8741922ac729cff8c57b52e218560
Maurice Reigman <m_c.r@hotmail.com>
2019-03-18 11:03:31 +0100

    - fixed bug in user service in backend

--- backend/src/features/users/services/users/users.service.ts
+++ backend/src/features/users/services/users/users.service.ts
@@ -11,8 +11,9 @@ export class UsersService {
 	constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) {}
 
 	public async saveUser(userEntity: UserEntity): Promise<UserEntity> {
-		const userExists = !! await this.userRepository.findOne({userName: userEntity.userName});
-		if (userExists) return this.userRepository.save(userEntity);
+		const userExists = !!(await this.userRepository.findOne({userName: userEntity.userName}));
+		console.log(userExists);
+		if (!userExists) return this.userRepository.save(userEntity);
 		else throw new HttpException('User already exists', HttpStatus.CONFLICT);
 	}
 
