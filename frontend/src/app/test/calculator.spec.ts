2d28649af61308bc78a276f5c80dfc2076374cd8
Maurice Reigman <m_c.r@hotmail.com>
2019-03-20 08:14:24 +0100

    added calculator example

---
+++ frontend/src/app/test/calculator.spec.ts
@@ -0,0 +1,12 @@
+import { Calculator } from './calculator';
+import { async, ComponentFixture, TestBed, fakeAsync, inject, tick} from '@angular/core/testing';
+
+
+
+fdescribe('FilmSearchComponent', () => {
+	let calculator: Calculator;
+
+    it('test', () => {
+        expect(true).toBeTruthy
+    })
+});
---
+++ frontend/src/app/test/calculator.ts
@@ -0,0 +1,15 @@
+export class Calculator {
+    public storage: number[] = []
+
+    public sum(a:number, b: number) {
+        return a + b
+    }
+
+    public multiply(a:number, b:number) {
+        return a * b
+    }
+
+    public devide(a:number, b:number) {
+        return a/b
+    }
+}
